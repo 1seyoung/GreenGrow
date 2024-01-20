a00a81ff521ff00ca4764b5189b1652a79090869
1seyoung <20171631@edu.hanbat.ac.kr>
2024-01-21 03:29:01 +0900

    project initial

---
+++ src/test/java/com/greengrow/plantdiary/PlantDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greengrow.plantdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
